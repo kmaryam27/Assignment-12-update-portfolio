a5bad9654a96563c736df114388b92e2bc98852b
k_maryam27 <k_maryam27@yahoo.com>
2018-12-17 18:02:11 -0500

    first version

---
+++ Portfolio/starter/assets/js/app.js
@@ -0,0 +1,70 @@
+const divClickList1 = document.getElementById('ptf_1');
+divClickList1.style.cursor = 'pointer';
+divClickList1.onclick = function() {
+    // location.href = "https://kmaryam27.github.io/Assignment2-ResponisiveLayouts-1/15-Stu_Practice1/Unsolved/";
+    window.open('https://kmaryam27.github.io/Assignment2-ResponisiveLayouts-1/15-Stu_Practice1/Unsolved/');
+};
+
+const divClickList2 = document.getElementById('ptf_2');
+divClickList2.style.cursor = 'pointer';
+
+divClickList2.onclick = function() {
+    // location.href = "https://kmaryam27.github.io/Assignment2-ResponisiveLayouts-2/16-Stu_Practice2/Unsolved/";
+    window.open('https://kmaryam27.github.io/Assignment2-ResponisiveLayouts-2/16-Stu_Practice2/Unsolved/');
+};
+
+const divClickList3 = document.getElementById('ptf_3');
+divClickList3.style.cursor = 'pointer';
+
+divClickList3.onclick = function() {
+    // location.href = "https://kmaryam27.github.io/Assignment2-ResponisiveLayouts-3/17-Stu_Practice3/Unsolved/";
+    window.open('https://kmaryam27.github.io/Assignment2-ResponisiveLayouts-3/17-Stu_Practice3/Unsolved/');
+};
+
+const divClickList4 = document.getElementById('ptf_4');
+divClickList4.style.cursor = 'pointer';
+
+divClickList4.onclick = function() {
+    // location.href = "https://kmaryam27.github.io/Assignment3-activity30/30-activity/unsolved/";
+    window.open('https://kmaryam27.github.io/Assignment3-activity30/30-activity/unsolved/');
+};
+
+const divClickList5 = document.getElementById('ptf_5');
+divClickList5.style.cursor = 'pointer';
+
+divClickList5.onclick = function() {
+    // location.href = "https://kmaryam27.github.io/Assignment3-activity30/30-activity/unsolved/";
+    window.open('https://amazon--hubblepuff.herokuapp.com/');
+};
+
+const divClickList6 = document.getElementById('ptf_6');
+divClickList6.style.cursor = 'pointer';
+
+divClickList6.onclick = function() {
+    // location.href = "https://kmaryam27.github.io/Assignment3-activity30/30-activity/unsolved/";
+    window.open('https://space-shooting-game.herokuapp.com/');
+};
+
+const divClickList7 = document.getElementById('ptf_7');
+divClickList7.style.cursor = 'pointer';
+
+divClickList7.onclick = function() {
+    // location.href = "https://kmaryam27.github.io/Assignment3-activity30/30-activity/unsolved/";
+    window.open('https://todolist-mongo-socketio.herokuapp.com/');
+};
+
+const divClickList8 = document.getElementById('ptf_8');
+divClickList8.style.cursor = 'pointer';
+
+divClickList8.onclick = function() {
+    // location.href = "https://kmaryam27.github.io/Assignment3-activity30/30-activity/unsolved/";
+    window.open('https://kmaryam27.github.io/HarryPotterWordGuess/HarryPotterWordGuess/');
+};
+
+const divClickList9 = document.getElementById('ptf_8');
+divClickList9.style.cursor = 'pointer';
+
+divClickList9.onclick = function() {
+    // location.href = "https://kmaryam27.github.io/Assignment3-activity30/30-activity/unsolved/";
+    window.open('https://kmaryam27.github.io/HarryPotterMemoryGame/HarryPotterMemoryGame/');
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment-12-update-portfolio
+add second group project to portfolio
